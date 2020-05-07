0206dd577fa75e28a57e04d971b48d5c7810d790
Darryl <williams.de39@gmail.com>
2020-05-06 21:02:51 -0700

    half of employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        console.log(`This employee's name is ${this.name}`)
+    }
+
+    getId() {
+        console.log(`This employee's ID is ${this.id}`)
+    }
+
+    getEmail() {
+        console.log(`This employee's name is ${this.email}`)
+    }
+}
+
+const e = new Employee("Darryl", 1134, "new@new.com")
+e.getName()
+module.exports = Employee
